53677d299def2d880f595cd0b09ebed61d257c5d
davidrules88 <davidbernstein@wr-130-64-97-35.medford.tufts.edu>
2014-10-25 00:57:11 -0400

    formula update, now does cool stuff

--- formula.py
+++ formula.py
@@ -1,14 +1,37 @@
 def formula(course):
-	return course.level / 100.0 + course.lab / 2.0 + course.length / 75.0
-
+	return course.level / 100.0 + course.haslab / 2.0 + course.length / 75.0
 
 class Course(object):
-	def __init__(self, level, lab, length):
+	def __init__(self, ID, level, title, haslab, length):
+		self.ID = ID
 		self.level = level
-		self.lab = lab
+		self.title = title
+		self.haslab = haslab
 		self.length = length
+	def thing(self):
+		print(ID,level,title,haslab,length)
 
 
-c = Course(20, True, 150)
+courseList = []
+size = 0
+courseList.append(Course("FAH 2",2, "Art History From 1700 to the Present", 0, 220))
+courseList.append(Course("FAH 15", 15, "Japanese Architecture", 0, 150))
+courseList.append(Course("FAH 19",19,"Classic Architecture",0,150))
+courseList.append(Course("FAH 21",21,"Early Islamic Art",0,150))
+courseList.append(Course("FAH 41",41,"The Age of Rembrandt and Bernini",0,150))
+courseList.append(Course("FAH 92",92,"Vikings!",0,150))
+courseList.append(Course("FAH 98",98,"Senior Integrative Project Seminar",0,150))
+courseList.append(Course("FAH 104",104,"Greek Art and Archaeology",0,150))
+courseList.append(Course("FAH 120",120,"Armenian Art and Shit",0,150))
+courseList.append(Course("FAH 122",122,"Iconoclosm and Shit",0,150))
+courseList.append(Course("FAH 193",193,"Histories of Modern Architecture",0,150))
+courseList.append(Course("FAH 200",200,"Narrative: Japanese Heroes",0,150))
+courseList.append(Course("FAH 280",280,"Photography in Mexico",0,150))
+courseList.append(Course("FAH 288",288,"Art!!!",0,150))
+size = 14
 
-print(formula(c))
+for x in range(0,size):
+	print "Name:",courseList[x].ID,courseList[x].title, "Score:",formula(courseList[x])
+	print
+	print "		Time (minutes):", courseList[x].length
+	print "______________"
